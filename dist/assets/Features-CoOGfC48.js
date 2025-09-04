380acfd7cd1e3e8d8eb36f6e95f1a1ed841c07be
Rakshit Tiwari <59415104+tiwarirakshit@users.noreply.github.com>
2025-09-04 11:06:09 +0530

    Update project structure and dependencies; add new assets and HTML template

---
+++ dist/assets/Features-CoOGfC48.js
@@ -0,0 +1,16 @@
+import{c as t,j as e}from"./index-DcyoFZ6k.js";/**
+ * @license lucide-react v0.451.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=t("Lightbulb",[["path",{d:"M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5",key:"1gvzjb"}],["path",{d:"M9 18h6",key:"x1upvd"}],["path",{d:"M10 22h4",key:"ceow96"}]]);/**
+ * @license lucide-react v0.451.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=t("Monitor",[["rect",{width:"20",height:"14",x:"2",y:"3",rx:"2",key:"48i651"}],["line",{x1:"8",x2:"16",y1:"21",y2:"21",key:"1svkeh"}],["line",{x1:"12",x2:"12",y1:"17",y2:"21",key:"vw1qmm"}]]);/**
+ * @license lucide-react v0.451.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=t("Pencil",[["path",{d:"M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z",key:"1a8usu"}],["path",{d:"m15 5 4 4",key:"1mk7zo"}]]),r=()=>{const i=[{icon:e.jsx(c,{className:"w-8 h-8"}),title:"Full-cycle support",description:"We will support you in producing a product that ticks all your boxes and meets your users' needs."},{icon:e.jsx(o,{className:"w-8 h-8"}),title:"Team as one",description:"Our team is made up of specialists from a range of fields. Their comprehensive approach takes care of everything from research and business analysis to design and development."},{icon:e.jsx(l,{className:"w-8 h-8"}),title:"Ahead of modernity",description:"Providing the most up-to-date UI is also a key focus for us. Check out our case studies to see how we've helped other businesses."}];return e.jsx("section",{className:"bg-white py-24",children:e.jsxs("div",{className:"max-w-[1400px] mx-auto px-6 md:px-12 lg:px-24",children:[e.jsx("div",{className:"max-w-3xl mb-20",children:e.jsx("h2",{className:"text-4xl md:text-5xl font-bold text-zinc-900 leading-tight",children:"By merging aesthetic design with reliable processes, we'll help you achieve your goals"})}),e.jsx("div",{className:"grid grid-cols-1 md:grid-cols-3 gap-12 md:gap-8",children:i.map((s,a)=>e.jsxs("div",{className:"flex flex-col gap-6",children:[e.jsx("div",{className:"w-12 h-12 flex items-center justify-center text-zinc-900",children:s.icon}),e.jsx("h3",{className:"text-2xl font-bold text-zinc-900",children:s.title}),e.jsx("p",{className:"text-lg text-zinc-600 leading-relaxed",children:s.description})]},a))})]})})};export{r as default};
